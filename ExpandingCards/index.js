93d44b1547ee5097ab5f8cd6fb9063c3ac87a5db
digwijay-tech <digwijay2@gmail.com>
2025-06-23 16:11:56 +0530

    Refactor HTML and CSS for improved readability; enhance JavaScript formatting

--- ExpandingCards/index.js
+++ ExpandingCards/index.js
@@ -1,16 +1,12 @@
-const panels =document.querySelectorAll('.panel')
-
-
-panels.forEach((panel)=>{
-    
-    panel.addEventListener('click',()=>{
-        removeActClass()
-        panel.classList.add('active')
-    })
-})
-
- function removeActClass(){
-    panels.forEach(panel=>{
-        panel.classList.remove('active')
-    })
- }
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActClass();
+    panel.classList.add("active");
+  });
+});
+function removeActClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
